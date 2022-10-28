ffbcd15eb76778d71a54eebb274f40866f8785cb
Rohit Kumar <rohitk@extensioncrm.com>
2022-10-28 14:30:45 +0000

    Update batch.py

--- core_erp/core_erp/customization/batch.py
+++ core_erp/core_erp/customization/batch.py
@@ -1,13 +1,5 @@
-
-from __future__ import unicode_literals
 import frappe
-from frappe import _
 from frappe.model.document import Document
-from frappe.model.naming import make_autoname, revert_series_if_last
-from frappe.utils import cint, flt, get_link_to_form
-from frappe.utils.data import add_days
-from frappe.utils.jinja import render_template
-from six import text_type
 class Batch(Document):
     def autoname(self):
         if self.reference_doctype =="Purchase Receipt":
